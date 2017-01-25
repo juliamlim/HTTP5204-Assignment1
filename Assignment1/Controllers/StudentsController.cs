f2b43969ed8853856a293c215fd9badf68061f05
juliamlim <juliamlim@hotmail.com>
2017-01-24 22:28:20 -0500

    Changes and file upload

---
+++ Assignment1/Controllers/StudentsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment1.Models;
+
+namespace Assignment1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        public ActionResult Julia()
+        {
+            Student me = new Student();
+            me.Name = "Julia M Lim";
+            me.Number = "n01156110";
+            me.Email = "juliamlim@hotmail.com";
+            me.Program = "Web Development";
+            return View(me);
+        }
+    }
+}
---
+++ Assignment1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Email { get; set; }
+        public string Program { get; set; } 
+    }
+}
